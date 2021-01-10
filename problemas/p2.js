bd2585b96b31ce174d494c82223f0ac54f2c872e
RicardoERR <reriverosr@gmail.com>
2021-01-09 23:42:13 -0300

    Se agrega libreria csv y fs para p2

--- problemas/p2.js
+++ problemas/p2.js
@@ -6,9 +6,28 @@
  */
 
 const Nodo = require("./src/Nodo");
+// const ParseCSV = require("./src/ParseCsv");
 const raiz = new Nodo("root", "Raíz");
 
+// const csv = new ParseCSV();
 //Agregar a raiz toda la estructura solicitada.
 //...
 
-console.log(raiz);
+// console.log(raiz);
+
+const parse = require('csv-parse')
+const fs = require('fs') 
+
+filename = './src/input-p2.csv';
+
+const data = []
+fs.createReadStream(filename)
+  .pipe(parse({ delimiter: ',' }))
+  .on('data', (r) => {
+    console.log(r);
+    data.push(r);        
+  })
+  .on('end', () => {
+    // console.log(data);
+    console.log('OK');
+  });
